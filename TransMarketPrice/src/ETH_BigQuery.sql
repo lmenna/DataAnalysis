082986229ad269aab717a7c5e8482a8dac08bab4
lmenna <louismenna@yahoo.com>
2018-11-12 09:12:21 -0500

    Initial commit

---
+++ TransMarketPrice/src/ETH_BigQuery.sql
@@ -0,0 +1,18 @@
+/* ETH_BigQuery.sql
+ * desc: Extracts daily transaction counts for Ethereum using the blocks table in google's BigQuery
+ *
+ */
+  
+select
+CAST(CEILING(UNIX_MILLIS(blocks.timestamp)/(1000*60*60*24)) as INT64) as IntDaysFrom19700101,
+min(blocks.timestamp) as MinTimestamp,
+max(blocks.timestamp) as MaxTimestamp,
+min(UNIX_MILLIS(blocks.timestamp)) as MinUnixTimestamp,
+max(UNIX_MILLIS(blocks.timestamp)) as MaxUnixTimestamp,
+min(blocks.number) as MinBlockNumber,
+max(blocks.number) as MaxBlockNumber,
+sum(blocks.transaction_count) as Transactions,
+sum(blocks.difficulty) as Difficulty
+from `bigquery-public-data.ethereum_blockchain.blocks` as blocks
+where blocks.number != 0
+group by IntDaysFrom19700101
